76c35712e53c2d1ba8024bb46733684cd4f68e4f
Lion Defi <80599635+liondefi@users.noreply.github.com>
2021-03-13 16:49:47 -0500

    Lion init

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,67 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: '/',
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    items: [
+      {
+        label: 'Exchange',
+        href: 'https://exchange.liondefi.com/',
+      },
+      {
+        label: 'Liquidity',
+        href: 'https://exchange.liondefi.com/#/pool',
+      },
+    ],
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: '/farms',
+  },
+  {
+    label: 'Nests',
+    icon: 'PoolIcon',
+    href: '/nests',
+  },
+  {
+    label: 'Info',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'PancakeSwap',
+        href: 'https://pancakeswap.info/token/0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
+      },
+      {
+        label: 'AstroTools',
+        href: 'https://app.astrotools.io/pancake-pair-explorer/0x19e7cbecdd23a16dfa5573df54d98f7caae03019',
+      },
+    ],
+  },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: 'Github',
+        href: 'https://github.com/goosedefi/',
+      },
+      {
+        label: 'Docs',
+        href: 'https://goosedefi.gitbook.io/goose-finance/',
+      },
+      {
+        label: 'Blog',
+        href: 'https://goosefinance.medium.com/',
+      },
+    ],
+  },
+]
+
+export default config
